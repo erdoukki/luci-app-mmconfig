77b6f1df140c1c9860399085f3e6379bcd0b8673
Konstantine Shevlakov <konst.shell@gmail.com>
2021-02-07 01:38:49 -0500

    :

--- luasrc/controller/modemconfig.lua
+++ luasrc/controller/modemconfig.lua
@@ -6,6 +6,6 @@ local utl = require "luci.util"
 
 function index()
 	entry({"admin", "modem"},  firstchild(), translate("Modem"), 45).dependent = false
-	entry({"admin", "modem", "modemconfig"}, cbi("modem/modemconfig"), translate("Modem config"), 9).leaf = true
+	entry({"admin", "modem", "modemconfig"}, cbi("modem/modemconfig"), translate("Band config"), 9).leaf = true
 end
 
