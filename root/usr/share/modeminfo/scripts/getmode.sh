9c6094c674ba955c1368e7fa3a2f353f4501f9f5
koshev-msk <66990040+koshev-msk@users.noreply.github.com>
2021-02-14 11:46:13 +0300

    luci-app-mmconfig: update getmode.sh
    * Search modem slot via mmcli

--- root/usr/share/modeminfo/scripts/getmode.sh
+++ root/usr/share/modeminfo/scripts/getmode.sh
@@ -2,7 +2,8 @@
 
 case $1 in
 	2g|3g|4g)
-		mmcli -J -m 0 | jsonfilter -e '@["modem"]["generic"]["supported-modes"][*]' | grep $1
+		SLOT=$(/usr/bin/mmcli -L | awk '{print $1}' | awk -F [\/] '{print $6}')
+		mmcli -J -m ${SLOT} | jsonfilter -e '@["modem"]["generic"]["supported-modes"][*]' | grep $1
 	;;
 esac
 
