1b4fa94b3e0e6a06d2fde59f4e07032dce62db01
koshev-msk <konst.shell@gmail.com>
2021-06-15 05:35:51 +0300

    getmode.sh: fix modem slot detect.

--- root/usr/share/modeminfo/scripts/getmode.sh
+++ root/usr/share/modeminfo/scripts/getmode.sh
@@ -2,7 +2,7 @@
 
 case $1 in
 	2g|3g|4g)
-		SLOT=$(/usr/bin/mmcli -L | awk '{print $1}' | awk -F [\/] '{print $6}')
+		SLOT=$(/usr/bin/mmcli -L | awk '{print $1}' | awk -F [\/] '{print $NF}')
 		mmcli -J -m ${SLOT} | jsonfilter -e '@["modem"]["generic"]["supported-modes"][*]' | grep $1
 	;;
 esac
